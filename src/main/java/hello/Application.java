683501eccb0edc529d15f3ea96453650570f9678
Renato Coutinho <renatoadsumus@gmail.com>
2018-08-20 21:24:25 -0300

    Forcando bular uma regra do Sonar

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,9 +3,29 @@ package hello;
 import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
+
+
+
 import org.springframework.boot.SpringApplication;
+
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+
 import org.springframework.context.ApplicationContext;
+
+
+
+
+
+
+
+
+
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
