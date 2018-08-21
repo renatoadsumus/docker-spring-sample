41e8f5859f3e7d22083809eb8cc9a793accc0068
renatoadsumus <renatoadsumus@gmail.com>
2018-08-21 12:51:18 -0300

    Executando Teste Unitario

---
+++ src/test/java/hello/HelloControllerTest.java
@@ -0,0 +1,34 @@
+package hello;
+
+import static org.junit.Assert.*;
+import java.net.URL;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerTest {
+
+
+    @Test
+    public void getHello() throws Exception {
+
+        String resultadoEsperado = "Greetings from Spring Boot!";
+        String resultadoAtual = "Greetings from Spring Boot!";
+
+        assertThat(resultadoAtual, is(equalTo(resultadoEsperado)));
+
+    }
+
+}
